1763b3785d71440749f8c0aad45bd8232615224b
fanolans <mailenolan@gmail.com>
2022-08-09 12:07:58 +0700

    create splashscreen

---
+++ lib/presentation/pages/splashscreen.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:cashstory/main.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTime() {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, navigationRoute);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  void navigationRoute() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) =>
+            const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Center(
+          child: Image.asset(
+            'assets/images/cashstory_splashscreen.png',
+            height: 300,
+            width: 300,
+          ),
+        ),
+      ),
+    );
+  }
+}
