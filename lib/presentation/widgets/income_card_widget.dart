bc831384dbb0575779b07a258ff2e50ef4ee4bcc
fanolans <mailenolan@gmail.com>
2022-08-11 15:49:08 +0700

    created a income card but it's not finished yet

---
+++ lib/presentation/widgets/income_card_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IncomeCard extends StatefulWidget {
+  const IncomeCard({Key? key}) : super(key: key);
+
+  @override
+  State<IncomeCard> createState() => _IncomeCardState();
+}
+
+class _IncomeCardState extends State<IncomeCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
